e86cf17dcfa548efabe85e02f0d7687fb97e8578
David Murillo <dmz866@hotmail.com>
2025-02-06 21:20:55 -0500

    examples

---
+++ bases/src/app.js
@@ -0,0 +1,5 @@
+const { emailTemplate } = require('./templates/01-template');
+
+require('./templates/01-template');
+
+console.log(emailTemplate);
---
+++ bases/src/templates/01-template.js
@@ -0,0 +1,9 @@
+const emailTemplate = `
+<div>
+<h1>Hi, {{name}}</h1>
+</div>
+`;
+
+module.exports = {
+    emailTemplate
+};
